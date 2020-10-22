c7493da2190a2a3b8591237a0305d97ca789cfcd
Sy Rashid <sy.rashid.11@gmail.com>
2020-10-22 10:53:07 +0300

    FEAT: Finished code

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,14 @@
+class MoviesController < ApplicationController
+  def index
+    if params[:query].present?
+      # sql_query = 'movies.title @@ :query
+      #   OR movies.syllabus @@ :query
+      #   OR directors.first_name @@ :query
+      #   OR directors.last_name @@ :query'
+      # @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%")
+      @movies = Movie.search(params[:query])
+    else
+      @movies = Movie.all
+    end
+  end
+end
